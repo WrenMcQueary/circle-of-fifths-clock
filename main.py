520ed3b8094fec82ed5f0888fc602409961f3606
Wren McQueary <wren.mcqueary@gmail.com>
2022-05-21 21:05:07 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+"""TODO: Docstring
+"""
+
+
+import argparse
+
+
+# TODO: Write readme
+# TODO: "Digital" version
+# TODO: "Analog" version
+
+
+if __name__ == "__main__":
+    # Parse arguments
+    parser = argparse.ArgumentParser(description="Joke idea")
