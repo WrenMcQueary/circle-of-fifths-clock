4e5642b2e07f9adcb171a20cb4cbae413fa94b40
Wren McQueary <wren.mcqueary@gmail.com>
2022-05-21 22:19:05 -0400

    Implemented analog animation

---
+++ drawers.py
@@ -0,0 +1,146 @@
+from datetime import datetime
+from math import pi, sin, cos
+
+
+def animation_loop(canvas_, render_in: str) -> None:
+    """Repeatedly update the canvas with a circle-of-fifths clock.
+    :param canvas_:       the tkinter canvas object
+    :param render_in:       either "analog" or "digital", to indicate the type of clock that should be drawn
+    """
+    # Handle errors
+    # render_in not a string
+    if not isinstance(render_in, str):
+        raise TypeError("render_in must be a string")
+    # render_in not "analog" or "digital"
+    if render_in not in ["analog", "digital"]:
+        raise ValueError("render_in must be 'analog' or 'digital'")
+
+    # Dictionary that converts from render_in value to the appropriate render function to use
+    render_in_to_render_function = {
+        "analog": render_analog,
+        "digital": render_digital
+    }
+
+    # Run
+    while True:
+        # Get the current time
+        current = datetime.now()
+        hour, minute, second = current.hour, current.minute, current.second
+
+        # Update the canvas using the appropriate function for render method
+        render_in_to_render_function[render_in](canvas_, hour, minute, second)
+
+
+def render_analog(canvas_in, hour_in: int, minute_in: int, second_in: int):
+    """Draw an animated clock in 'analog' style
+    """
+    # Handle errors
+    # hour_in not an int
+    if not isinstance(hour_in, int):
+        raise TypeError("hour_in must be an int")
+    # minute_in not an int
+    if not isinstance(minute_in, int):
+        raise TypeError("minute_in must be an int")
+    # second_in not an int
+    if not isinstance(second_in, int):
+        raise TypeError("second_in must be an int")
+    # hour_in not a value from 0 through 23
+    if not 0 <= hour_in <= 23:
+        raise ValueError("hour_in must be somewhere from 0 through 23")
+    # minute_in not a value from 0 through 59
+    if not 0 <= minute_in <= 59:
+        raise ValueError("minute_in must be somewhere from 0 through 59")
+    # second_in not a value from 0 through 59
+    if not 0 <= second_in <= 59:
+        raise ValueError("second_in must be somewhere from 0 through 59")
+
+    # Basic constants
+    guide_offset = 425  # Pixels
+    guide_length = 450  # Pixels
+    hour_hand_length = 200  # Pixels
+    minute_hand_length = 400  # Pixels
+    second_hand_length = 400  # Pixels
+
+    # Conversions from angle ii to sharps/flats
+    angle_ii_to_sharps_flats = ["3#", "2#", "1#", "", "1b", "2b", "3b", "4b", "5b", "6#", "5#", "4#"]
+
+    # Conversions from angle ii to major
+    angle_ii_to_major = ["A", "D", "G", "C", "F", "Bb", "Eb", "Ab", "Db", "F#", "B", "E"]
+
+    # Conversions from angle ii to minor
+    angle_ii_to_minor = ["F#", "B", "E", "A", "D", "G", "C", "F", "Bb", "Eb", "G#", "C#"]
+
+    # Conversions from angle ii to roman numeral
+    angle_ii_to_roman_numeral = ["vi", "ii", "V", "I", "IV", "", "", "", "", "", "vii", "iii"]
+
+    # Wipe the canvas
+    canvas_in.delete("all")
+
+    # Draw the circle of the clock
+    canvas_in.create_oval(0, 0, 900, 900, outline="black")
+
+    # Draw hour guides
+    for ii in range(12):
+        angle = ii / 12 * 2 * pi
+        canvas_in.create_line(450 + guide_offset * cos(angle), 450 - guide_offset * sin(angle), 450 + guide_length * cos(angle), 450 - guide_length * sin(angle), fill="gray")
+
+    # Draw sharps/flats markings
+    for ii in range(12):
+        angle = ii / 12 * 2 * pi
+        canvas_in.create_text(450 + 400 * cos(angle), 450 - 400 * sin(angle), text=angle_ii_to_sharps_flats[ii], font=("Helvetica", 18), fill="gray")
+
+    # Draw major markings
+    for ii in range(12):
+        angle = ii / 12 * 2 * pi
+        canvas_in.create_text(450 + 350 * cos(angle), 450 - 350 * sin(angle), text=angle_ii_to_major[ii], font=("Helvetica", 36), fill="black")
+
+    # Draw minor markings
+    for ii in range(12):
+        angle = ii / 12 * 2 * pi
+        canvas_in.create_text(450 + 250 * cos(angle), 450 - 250 * sin(angle), text=angle_ii_to_minor[ii], font=("Helvetica", 24), fill="gray")
+
+    # Draw roman numerals
+    for ii in range(12):
+        angle = ii / 12 * 2 * pi
+        canvas_in.create_text(450 + 150 * cos(angle), 450 - 150 * sin(angle), text=angle_ii_to_roman_numeral[ii], font=("Helvetica", 18), fill="blue")
+
+    # Draw the hour hand
+    hour_angle = pi / 2 - (hour_in / 12 * 2 * pi) - (minute_in / 60 / 12 * 2 * pi)
+    canvas_in.create_line(450, 450, 450 + hour_hand_length * cos(hour_angle), 450 - hour_hand_length * sin(hour_angle), fill="black")
+
+    # Draw the minute hand
+    minute_angle = pi / 2 - (minute_in / 60 * 2 * pi)
+    canvas_in.create_line(450, 450, 450 + minute_hand_length * cos(minute_angle), 450 - minute_hand_length * sin(minute_angle), fill="black")
+
+    # Draw the second hand
+    second_angle = pi / 2 - (second_in / 60 * 2 * pi)
+    canvas_in.create_line(450, 450, 450 + second_hand_length * cos(second_angle), 450 - second_hand_length * sin(second_angle), fill="red")
+
+    # Update the canvas
+    canvas_in.update()
+
+
+def render_digital(canvas_in, hour_in: int, minute_in: int, second_in: int):
+    """Draw an animated clock in 'digital' style.
+    """
+    # Handle errors
+    # hour_in not an int
+    if not isinstance(hour_in, int):
+        raise TypeError("hour_in must be an int")
+    # minute_in not an int
+    if not isinstance(minute_in, int):
+        raise TypeError("minute_in must be an int")
+    # second_in not an int
+    if not isinstance(second_in, int):
+        raise TypeError("second_in must be an int")
+    # hour_in not a value from 0 through 23
+    if not 0 <= hour_in <= 23:
+        raise ValueError("hour_in must be somewhere from 0 through 23")
+    # minute_in not a value from 0 through 59
+    if not 0 <= minute_in <= 59:
+        raise ValueError("minute_in must be somewhere from 0 through 59")
+    # second_in not a value from 0 through 59
+    if not 0 <= second_in <= 59:
+        raise ValueError("second_in must be somewhere from 0 through 59")
+
+    pass    # TODO: Implement
--- main.py
+++ main.py
@@ -1,15 +1,38 @@
-"""TODO: Docstring
+"""Joke idea.  Virtual wall clock that uses the circle of fifths instead of ordinary numbers.
+Command line arguments are as follows:
+-r, --render:       determines whether the rendered image of the clock is 'analog' or 'digital'
 """
 
 
 import argparse
+import tkinter
+from drawers import animation_loop
 
 
 # TODO: Write readme
 # TODO: "Digital" version
-# TODO: "Analog" version
 
 
 if __name__ == "__main__":
     # Parse arguments
-    parser = argparse.ArgumentParser(description="Joke idea")
+    parser = argparse.ArgumentParser(description="Joke idea. Virtual wall clock that uses the circle of fifths instead of ordinary numbers")
+    parser.add_argument("--render", "-r", action="store", type=str, required=False, default="analog", help="determines whether the rendered image of the clock is 'analog' or 'digital'", dest="render")
+    args = parser.parse_args()
+    render = args.render
+
+    # Handle errors
+    # render not "analog" or "digital"
+    if render not in ["analog", "digital"]:
+        raise ValueError("render must be 'analog' or 'digital'")
+
+    # Begin a tkinter window with a canvas
+    window = tkinter.Tk()
+    window.title("Circle of fifths clock")
+    window.geometry = "900x900"
+
+    my_canvas = tkinter.Canvas(window, width=900, height=900, background="white")
+    my_canvas.grid(row=0, column=0)
+
+    # Repeatedly get the time and draw it
+    my_canvas.after(1000, lambda: animation_loop(my_canvas, render))
+    window.mainloop()
